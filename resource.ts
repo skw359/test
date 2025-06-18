e559b63c3ebcdfba1b6d28fca9c3c4f6edfba217
Mason <75508453+skw359@users.noreply.github.com>
2025-06-18 11:07:40 -0400

    Update resource.ts

--- resource.ts
+++ resource.ts
@@ -1,5 +1,6 @@
+// amplify/data/resource.ts
+
 import { a, defineData, ClientSchema } from '@aws-amplify/backend';
-import { timeStamp } from 'console';
 
 const schema = a.schema({
   Message: a
@@ -12,13 +13,12 @@ const schema = a.schema({
     .authorization((allow) => [
       allow.authenticated(), 
     ])
-    // Add indexes for efficient querying
     .secondaryIndexes((index) => [
       index('owner').sortKeys(['timestamp']),
       index('receiver').sortKeys(['timestamp']),
     ]),
 
-  Chat: a //tables stores the unique chat id for each chat. Also stores the name of chat and when the initial chat was created
+  Chat: a
     .model({
       createdAt: a.datetime().required(),
       name: a.string()
@@ -27,7 +27,7 @@ const schema = a.schema({
       allow.authenticated(), 
     ]),
 
-  ChatUser: a // the join table between the chat table and the users in cognito. (what users are a part of what chats?)
+  ChatUser: a
     .model({
       username: a.string().required(),
       chatId: a.string().required(),
@@ -41,53 +41,51 @@ const schema = a.schema({
       index('chatId'),
     ]),
 
-    ChatMessages: a // this table stores chats sent by users. It knows what chat room the message is from, when it was sent and who sent it
-      .model({
-        chatId: a.string().required(),
-        sendername: a.string().required(),
-        content: a.string().required(),
-        timestamp: a.string().required(),
-        messageType: a.string().default('text'),
-      })
-      .authorization((allow) => [
+  ChatMessages: a
+    .model({
+      chatId: a.string().required(),
+      sendername: a.string().required(),
+      content: a.string().required(),
+      timestamp: a.string().required(),
+      messageType: a.string().default('text'),
+    })
+    .authorization((allow) => [
       allow.authenticated(),  
     ]),
 
-    // calendar/meeting related tables
-    MeetingInvitation: a
-      .model({
-        // remove invitationid because dynamodb already has a unique key, it's auto generated
-        chatId: a.string().required(),
-        createdBy: a.string().required(),
-        title: a.string().required(),
-        description: a.string(),
-        dateTime: a.datetime().required(),
-        location: a.string(),
-        status: a.string().required().default('pending'), // it can be ppending, accepted, declined, cancelled (response can update this)
-        createdAt: a.datetime().required(),
-        updatedAt: a.datetime(),
-        invited: a.string().required(),
-      })
-      .authorization((allow) => [
-        allow.authenticated(),
-      ])
-      .secondaryIndexes((index) => [
-        index('chatId').sortKeys(['dateTime']),
-        index('createdBy').sortKeys(['createdAt']),
-      
-      ]),
+  MeetingInvitation: a
+    .model({
+      chatId: a.string().required(),
+      createdBy: a.string().required(),
+      title: a.string().required(),
+      description: a.string(),
+      dateTime: a.datetime().required(),
+      location: a.string(),
+      status: a.string().required().default('pending'),
+      createdAt: a.datetime().required(),
+      updatedAt: a.datetime(),
+      invited: a.string().required(),
+    })
+    .authorization((allow) => [
+      allow.authenticated(),
+    ])
+    .secondaryIndexes((index) => [
+      index('chatId').sortKeys(['dateTime']),
+      index('createdBy').sortKeys(['dateTime']),
+      index('invited').sortKeys(['dateTime']), 
+    ]),
 
-      UserProfile: a
-      .model({
-        userId: a.string().required(),
-        prefUser: a.string().required(),
-      })
-      .authorization((allow)=> [
-        allow.authenticated(),
-      ])
-      .secondaryIndexes((index) => [
-        index('userId')
-      ]),
+  UserProfile: a
+    .model({
+      userId: a.string().required(),
+      prefUser: a.string().required(),
+    })
+    .authorization((allow)=> [
+      allow.authenticated(),
+    ])
+    .secondaryIndexes((index) => [
+      index('userId')
+    ]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -98,32 +96,3 @@ export const data = defineData({
     defaultAuthorizationMode: 'userPool',
   },
 });
-
-/*== STEP 2 ===============================================================
-Go to your frontend source code. From your client-side code, generate a
-Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
-WORK IN THE FRONTEND CODE FILE.)
-
-Using JavaScript or Next.js React Server Components, Middleware, Server
-Actions or Pages Router? Review how to generate Data clients for those use
-cases: https://docs.amplify.aws.gen2/build-a-backend/data/connect-to-API/
-=========================================================================*/
-
-/*
-"use client"
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-
-const client = generateClient<Schema>() // use this Data client for CRUDL requests
-*/
-
-/*== STEP 3 ===============================================================
-Fetch records from the database and use them in your frontend component.
-(THIS SNIPPET WILL ONLY WORK IN THE FRONTEND CODE FILE.)
-=========================================================================*/
-
-/* For example, in a React component, you can use this snippet in your
-  function's RETURN statement */
-// const { data: todos } = await client.models.Todo.list()
-
-// return <ul>{todos.map(todo => <li key={todo.id}>{todo.content}</li>)}</ul>
